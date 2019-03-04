7f3743418bb3b8300dc3b8a2c02a0e832e72938c
SamLegrand <37298570+SamLegrand@users.noreply.github.com>
2019-03-04 18:48:09 +0100

    Added app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-### TUTORIAL Len Feremans
-###see tutor https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
 from flask import Flask
-from flask.templating import render_template
-from flask import request, session, jsonify
+from flask import render_template
+from flask import jsonify, session, request, redirect
 
-### INITIALIZE SINGLETON SERVICES ###
-app = Flask('scoliomore')
+app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return 'Hello World'
+@app.route('/')
+def render_home():
+    return 'Hello World!'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
